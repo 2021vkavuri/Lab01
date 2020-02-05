38cf72e69ec72bffd8d9b99f970f894b941b0191
vinaykavuri <kavurivinay@gmail.com>
2020-02-05 15:02:40 -0500

    buttonsssss

--- app/src/main/java/com/kavurivinay/lab01/MainActivity.java
+++ app/src/main/java/com/kavurivinay/lab01/MainActivity.java
@@ -1,14 +1,22 @@
 package com.kavurivinay.lab01;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
+import android.widget.Button;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button myButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myButton = findViewById(R.id.button);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                System.out.println("yay");
+            }
+        });
     }
 }
