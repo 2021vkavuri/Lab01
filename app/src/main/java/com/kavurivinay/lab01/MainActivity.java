87869069a31e8baaf9b00f3110dd4237826e785f
vinaykavuri <kavurivinay@gmail.com>
2020-02-05 15:56:23 -0500

    buttonsssss

--- app/src/main/java/com/kavurivinay/lab01/MainActivity.java
+++ app/src/main/java/com/kavurivinay/lab01/MainActivity.java
@@ -1,22 +1,34 @@
 package com.kavurivinay.lab01;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
 import android.view.View;
 import android.widget.Button;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button myButton;
+    TextView myText;
+    int counter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        counter = 0;
         myButton = findViewById(R.id.button);
+        myText = findViewById(R.id.textView);
         myButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
-                System.out.println("yay");
+                counter++;
+                myText.setText("" + counter);
             }
         });
+    }
+    public void doAction(View v)
+    {
+
     }
 }
