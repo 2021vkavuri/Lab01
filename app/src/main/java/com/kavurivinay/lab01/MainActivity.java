d87b592c8ac5a3dd04c8bf03b0411df14008e26c
vinaykavuri <kavurivinay@gmail.com>
2020-02-07 13:59:28 -0500

    buttonsssss

--- app/src/main/java/com/kavurivinay/lab01/MainActivity.java
+++ app/src/main/java/com/kavurivinay/lab01/MainActivity.java
@@ -3,6 +3,9 @@ package com.kavurivinay.lab01;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.res.Resources;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.os.Bundle;
@@ -10,18 +13,23 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button myButton;
+    Button myButton2;
     TextView myText;
     int counter;
+    boolean doLoop = false;
+    final Handler h = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         counter = 0;
         myButton = findViewById(R.id.button);
+        myButton2 = findViewById(R.id.button2);
         myText = findViewById(R.id.textView);
         myButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
+                Log.i("app","Increment Counter");
                 counter++;
                 myText.setText("" + counter);
             }
@@ -29,6 +37,41 @@ public class MainActivity extends AppCompatActivity {
     }
     public void doAction(View v)
     {
+        final int width = Resources.getSystem().getDisplayMetrics().widthPixels;
+        final int height = Resources.getSystem().getDisplayMetrics().heightPixels - 200;
+        doLoop = !doLoop;
+        if(doLoop)
+        {
+            counter = 0;
+            myText.setText("" + counter);
+            myButton.setClickable(true);
+            Log.i("app","Start of Game");
+        }
+        else{
+            myButton.setClickable(false);
+            Log.i("app","End of Game");
+        }
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                myButton2.setX((float) Math.random() * (width - myButton2.getWidth()));
+                myButton2.setY((float) Math.random() * (height - myButton2.getHeight()));
 
+                myButton.setX((float) Math.random() * (width - myButton.getWidth()));
+                myButton.setY((float) Math.random() * (height - myButton.getHeight()));
+                if(doLoop)
+                {
+                    h.postDelayed(this, 600);
+                }
+                else{
+                    h.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            Log.i("app", "Waiting 1 Second");
+                        }
+                    }, 1000);
+                }
+            }
+        }, 600);
     }
 }
